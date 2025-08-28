7f7bba10ef2b42abc81eb5153f7fb644db39ff16
Azaz Mohammad Kaif <azazkaif3@gmail.com>
2025-08-28 16:23:55 +0600

    Login and Signup Pge frontend by 22-48795-3

---
+++ Login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div class="main-content">
+    <header>
+    <h2>ALIDADA</h2>
+    <nav>
+        <a href="Login.php">Home</a>
+        <a href="../html/Education.html">Contact</a>
+        <a href="../html/Experience.html">About</a>
+        <a href="index.php">Sign Up</a>
+    </nav>
+</header>
+
+    <hr>
+    </div>
+<img src="Image/LOGINSIGNUP.jpg" alt="signup" >
+<form method="GET">
+
+
+
+    
+
+
+
+        <div class="container">
+            <h1>Log In</h1>
+            
+        </div>
+
+        <div class="container">
+            
+            <input type="text" id="name" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="name"> <strong>Name</strong></label>
+        </div>
+
+
+        <div class="container">
+            <input type="password" id="password" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="password"><strong>Password</strong></label>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn">Log In</button>
+        </div>
+
+
+        
+
+        
+        
+    </form>
+    
+
+    <footer>
+        
+        <hr>
+        <p>Copyright &copy; All rights reserved by ALIDADA</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div class="main-content">
+    <header>
+    <h2>ALIDADA</h2>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="../html/Education.html">Contact</a>
+        <a href="../html/Experience.html">About</a>
+        <a href="Login.php">Log In</a>
+    </nav>
+</header>
+
+    <hr>
+    </div>
+<img src="Image/LOGINSIGNUP.jpg" alt="signup" >
+<form method="GET">
+
+
+
+    
+
+
+
+        <div class="container">
+            <h1>Sign Up</h1>
+            
+        </div>
+
+        <div class="container">
+            
+            <input type="text" id="name" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="name"> <strong>Name</strong></label>
+        </div>
+
+
+        <div class="container">
+            
+            <input type="email" id="email" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="email"> <strong>Email</strong></label>
+        </div>
+
+        <div class="container">
+            
+            <input type="text" id="number" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="text"> <strong>Mobile</strong></label>
+        </div>
+
+        <div class="container">
+            
+            <input type="text" id="address" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="address"> <strong>Address</strong></label>
+        </div>
+
+        <div class="container">
+            <input type="password" id="password" autocomplete="off" placeholder=" " class="form_input" required>
+            <label for="password"><strong>Password</strong></label>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn">Sigh Up</button>
+        </div>
+
+
+        
+
+        
+        
+    </form>
+    
+
+    <footer>
+        
+        <hr>
+        <p>Copyright &copy; All rights reserved by ALIDADA</p>
+    </footer>
+</body>
+</html>
