ff0e0845134e35e6601ce463fcc4a225008f0a21
Azaz Mohammad Kaif <azazkaif3@gmail.com>
2025-09-03 02:56:15 +0600

    Validation

--- Customer/cusProfile.php
+++ Customer/cusProfile.php
@@ -1,36 +1,35 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['userId'])) {
+    header("Location: ../Login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Customer Profile</title>
     <link rel="stylesheet" href="../CSS/cusProfile.css">
 </head>
 <body>
     <div class="main-content">
-    <header>
-    <h2>ALIDADA</h2>
+        <header>
+            <h2>ALIDADA</h2>
+            <nav>
+                <a href="cusDashboard.php">Home</a>
+                <a href="logout.php">Log Out</a>
+            </nav>
+        </header>
 
-     
-    <nav>
-        
-        <a href="cusDashboard.php">Home</a>
-        <a href="../Login.php">Log Out</a>
-
-    </form>
-    </nav>
-
-   
-</header>
-
-    <hr>
-    
-
-
-    <footer>
-        
         <hr>
-        <p>Copyright &copy; All rights reserved by ALIDADA</p>
-    </footer>
+        
+        <footer>
+            <hr>
+            <p>Copyright &copy; All rights reserved by ALIDADA</p>
+        </footer>
+    </div>
 </body>
 </html>
