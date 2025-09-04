6bf7b1a93cec414cb3c4e0adb28c1b3303429f6b
Azaz Mohammad Kaif <azazkaif3@gmail.com>
2025-09-05 00:28:16 +0600

    Admin PHP, CSS, VALIDATION, BACKEND

--- Admin/adDashboard.php
+++ Admin/adDashboard.php
@@ -1,3 +1,33 @@
-<?php
-    echo "i am admin";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/adDashboard.css">
+    
+</head>
+<body>
+    <div class="main-content">
+    <header>
+        <h2>ALIDADA</h2>
+
+        
+        <nav>
+            
+            <a href="adDashboard.php">Home</a>
+            <a href="customerInfo.php">Customer Information</a>
+            <a href="adProfile.php">Profile</a>
+        </nav>
+
+   
+    </header>
+
+
+    <footer>
+        
+        <hr>
+        <p>Copyright &copy; All rights reserved by ALIDADA</p>
+    </footer>
+</body>
+</html>
---
+++ Admin/customerInfo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Profile</title>
+    <link rel="stylesheet" href="../CSS/customerInfo.css">
+</head>
+<body>
+    <div class="main-content">
+        <header>
+            <h2>ALIDADA</h2>
+            <nav>
+                <a href="adDashboard.php">Home</a>
+                <a href="adProfile.php">Profile</a>
+                <a href="logout.php">Log Out</a>
+            </nav>
+        </header>
+
+        <hr>
+
+
+        <footer>
+            <hr>
+            <p>Copyright &copy; All rights reserved by ALIDADA</p>
+        </footer>
+    </div>
+</body>
+</html>
