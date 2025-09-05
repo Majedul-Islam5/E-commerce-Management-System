13f07958234e5455fc0517bb91b0fab143fcedab
Majedul-Islam5 <mohammadanas241@gmail.com>
2025-09-05 10:57:27 +0600

    implemented addProductValidation

--- Admin/adAddProduct.php
+++ Admin/adAddProduct.php
@@ -1,3 +1,18 @@
+<?php
+
+    session_start();
+
+    include_once ('../Database/data.php');
+
+    if(!isset($_SESSION['userId'])) 
+    {
+        header("Location: ../Login.php");
+        exit();
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +34,80 @@
         </header>
 
         <hr>
+        
+       <form action="addProductValidation.php" method="POST" enctype="multipart/form-data">
+
+            <div class="container">
+                <label for="p_name"><strong>Product Name: </strong></label>
+                <input type="text" id="p_name" name="p_name" autocomplete="off" placeholder=" " class="form_input">
+                <span id="pnameErr">
+                    <?php
+                    if (isset($_SESSION['p_name'])) {
+                        echo $_SESSION['p_name'];
+                        unset($_SESSION['p_name']);
+                    }
+                    ?>
+                </span>
+            </div>
+
+            <div class="container">
+                <label for="price"><strong>Price: </strong></label>
+                <input type="text" id="price" name="price" autocomplete="off" placeholder=" " min="1" class="form_input">
+                <span id="priceErr">
+                    <?php
+                    if (isset($_SESSION['price'])) {
+                        echo $_SESSION['price'];
+                        unset($_SESSION['price']);
+                    }
+                    ?>
+                </span>
+            </div>
+
+
+            <div class="container">
+                <label for="product"><strong>Product Image: </strong></label>
+                <input type="file" id="product" name="product" autocomplete="off" placeholder=" " class="form_input">
+                <span id="productErr">
+                    <?php
+                    if (isset($_SESSION['product'])) {
+                        echo $_SESSION['product'];
+                        unset($_SESSION['product']);
+                    }
+                    ?>
+                </span>
+            </div>
+
+            <div class="container">
+                <label for="stock"><strong>Stock: </strong></label>
+                <input type="text" id="stock" name="stock" autocomplete="off" placeholder=" " class="form_input">
+                <span id="stockErr">
+                    <?php
+                    if (isset($_SESSION['stock'])) {
+                        echo $_SESSION['stock'];
+                        unset($_SESSION['stock']);
+                    }
+                    ?>
+                </span>
+            </div>
+
+            <div class="container">
+                <label for="category"><strong>Category: </strong></label>
+                <input type="text" id="category" name="category" autocomplete="off" placeholder=" " class="form_input">
+                <span id="categoryErr">
+                    <?php
+                    if (isset($_SESSION['category'])) {
+                        echo $_SESSION['category'];
+                        unset($_SESSION['category']);
+                    }
+                    ?>
+                </span>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn">Add Product</button>
+            </div>
+
+       </form>
 
 
         <footer>
---
+++ Admin/addProductValidation.php
@@ -0,0 +1,165 @@
+<?php
+
+    session_start();
+
+    include_once ('../Database/data.php');
+
+    if(!isset($_SESSION['userId'])) 
+    {
+        header("Location: ../Login.php");
+        exit();
+    }
+
+    $result = $conn->query("select * from product");
+    $result=$result->fetch_all(MYSQLI_ASSOC);
+
+    function test_input($data) 
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $p_name=isset($_POST['p_name']) ? test_input($_POST['p_name']) : '';
+    $price=isset($_POST['price']) ? test_input($_POST['price']) : '';
+    $stock=isset($_POST['stock']) ? test_input($_POST['stock']) : '';
+    $category=isset($_POST['category']) ? test_input($_POST['category']) : '';
+
+
+    $errors="";
+    if(empty($p_name)) 
+    {
+        $_SESSION['p_name'] = "Product name is empty";
+        $errors="empty";
+    }
+    else
+    {
+        if(!preg_match("/^(?=(?:.*[a-zA-Z]){4,})[a-zA-Z ]+$/", $p_name)) 
+        {
+            $_SESSION['p_name']="Product name can contain only letters";
+            $errors="empty";
+        }
+        if(strlen($p_name)<4) 
+        {
+            $_SESSION['p_name']="Product name must be atleast 4 letters";
+            $errors="empty";
+        }
+
+        foreach($result as $row)
+        {
+            if($row['p_name']==$p_name)
+            {
+                $_SESSION['p_name'] = "This Product Already Exits";
+                $errors="empty";
+            }
+        }
+    }
+
+    if(empty($price)) 
+    {
+        $_SESSION['price']="Add the price of product";
+        $errors="empty";
+    }
+    else
+    {
+        if(!preg_match("/^-?\d+$/", $price)) 
+        {
+            $_SESSION['price']="Price must be a valid integer";
+            $errors="empty";
+        }
+
+        if($price<1) 
+        {
+            $_SESSION['price']="Price Must be Greater than 0";
+            $errors="empty";
+        }
+    }
+
+    if(empty($stock)) 
+    {
+        $_SESSION['stock']="Add the Quantity of product";
+        $errors="empty";
+    }
+    else
+    {
+        if(!preg_match("/^-?\d+$/", $stock)) 
+        {
+            $_SESSION['stock'] = "Quantity must be a valid integer";
+            $errors="empty";
+        }
+        if($stock <1) 
+        {
+            $_SESSION['stock'] = "Quantity Must be Greater than 0";
+            $errors="empty";
+        }
+    }
+
+    if(empty($category)) 
+    {
+        $_SESSION['category'] = "Category is empty";
+        $errors="empty";
+    }
+    else
+    {
+        if(!preg_match("/^[a-zA-Z]+$/", $category)) 
+        {
+            $_SESSION['category'] = "Category name can contain only letters";
+            $errors="empty";
+        }
+        if(strlen($category) < 2) 
+        {
+            $_SESSION['category'] = "Category must be atleast 2 letters";
+            $errors="empty";
+        }
+    }
+
+    if (!isset($_FILES['product']) ||  $_FILES['product']['error'] !== UPLOAD_ERR_OK) 
+    {
+    
+        $_SESSION['product'] = "Please upload a product image.";
+        $errors="empty";
+    }
+    else
+    {
+        $fileName=$_FILES["product"]["name"];
+        $folder="../Image/".$fileName;
+
+        if (file_exists($folder)) 
+        {
+            $_SESSION['product'] = "This file already exists. Rename your file.";
+            $errors="empty";
+        }
+
+        foreach($result as $row)
+        {
+            if($row['image_url']==$fileName)
+            {
+                $_SESSION['product'] = "This file already exists. Rename your file.";
+                $errors="empty";
+            }
+        }
+    }
+
+    if (!empty($errors)) 
+    {
+        header("Location: adAddProduct.php");
+        exit();
+    }
+    else
+    {
+        
+        move_uploaded_file($_FILES["product"]["tmp_name"],$folder);
+
+        $stmt=$conn->prepare("insert into product (p_name,price,image_url,stock,category) values (?,?,?,?,?)");
+        $stmt->bind_param("sdsis",$p_name,$price,$fileName,$stock,$category);
+        $stmt->execute();
+
+        header("Location: adAddProduct.php");
+        
+    }
+
+
+
+
+?>
