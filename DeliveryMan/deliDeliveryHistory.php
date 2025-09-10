8ce2d92587377c7fc89d397b3ac9f84b8d213b59
Majedul-Islam5 <mohammadanas241@gmail.com>
2025-09-10 12:17:43 +0600

    added the deliveryHistory check feature

--- DeliveryMan/deliDeliveryHistory.php
+++ DeliveryMan/deliDeliveryHistory.php
@@ -1,3 +1,40 @@
+<?php
+
+    session_start();
+
+    include_once ('../Database/data.php');
+
+    if(!isset($_SESSION['userId'])) 
+    {
+        header("Location: ../Login.php");
+        exit();
+    }
+    
+    $userId=$_SESSION['userId'];
+
+    $visi1="";
+    $visi2="";
+
+
+    $result=$conn->query("select * from order_info where d_id='$userId' and status='delivered'"); 
+    $result=$result->fetch_all(MYSQLI_ASSOC);
+    if(count($result)==0)
+    {
+        $visi1="block";
+        $visi2="none";
+    }
+
+    else
+    {
+        $cusresult=$conn->query("select order_info.order_id, user_info.user_name, user_info.address, user_info.nid from user_info INNER JOIN order_info on user_info.user_id=order_info.c_id where order_info.d_id='$userId' and status='delivered'"); 
+        $cusresult=$cusresult->fetch_all(MYSQLI_ASSOC);
+                
+        $visi1="none";
+        $visi2="block";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +56,45 @@
         </header>
 
         <hr>
+
+        <div class="no-data" style="display: <?php echo($visi1)?>;">
+            <h1>Nothing Delivered Yet</h1>
+        </div>
+
+
+        <div style="display: <?php echo($visi2)?>;" class="product">
+
+        <table>
+            <tr>
+                <th>User Name</th>
+                <th>Address</th>
+                <th>Payment</th>
+            
+            </tr>
+                <?php
+
+                foreach($cusresult  as $row):
+                    $order_id=$row['order_id'];
+                    $user_name=$row['user_name'];
+                    $address=$row['address'];
+                    $nid=$row['nid'];
+                
+                ?>
+                <tr>
+                    <td><?php echo($user_name)?></td>
+                    <td><?php echo($address)?></td>
+                    <td>10k</td>
+                </tr>
+            <?php endforeach;?>
+            <tr>
+                <td></td>
+                <td>Total earnings</td>
+                <td>1000k</td>
+            </tr>
+                
+        </table>
+
+    </div>
 
 
         <footer>
